b7121acf4202b8b66c2ac423e8e6c808183638a9
MatheuZin777 <matheusxaviersousa38@gmail.com>
2024-07-02 10:26:24 -0300

    Update validacao.php

--- recusando.php
+++ recusando.php
@@ -6,52 +6,99 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
 </head>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .bord {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 90vh;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 300px;
+        padding: 20px;
+        background-color: #f2f2f2;
+        border-radius: 5px;
+    }
+
+    form input {
+        width: 100%;
+        padding: 10px;
+        margin-bottom: 10px;
+    }
+
+    form label {
+        margin-bottom: 15px;
+    }
+
+    h1{
+        margin: 10px;
+        margin-right: 200px;
+    }
+
+    #btn{
+        background-color: #4CAF50;
+        color: white;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+        margin-top: 10px;
+        transition-duration: 0.4s;
+        border-radius: 10px;
+    }
+</style>
 
 <body>
-    <form method="post">
-        <input type="text" placeholder="Nome" name="Nome">
-        <input type="number" placeholder="Idade" name="Idade">
-        <label>Sexo:
-            <select>
-                <option></option>
-                <option>
-                    masculino
-                </option>
-                <option name="feminino">
-                    feminino
-                </option>
-            </select>
-        </label>
-        <input type="submit" value="Enviar">
-    </form>
-
+    <div class="bord">
+        <form method="post">
+            <h1>Log</h1>
+            <input type="text" placeholder="Nome" name="Nome">
+            <input type="number" placeholder="Idade" name="Idade">
+            <label>Sexo:
+                <select name="sexo">
+                    <option value="">Selecione</option>
+                    <option value="masculino">Masculino</option>
+                    <option value="feminino">Feminino</option>
+                </select>
+            </label>
+            <input id="btn" type="submit" value="Enviar">
+        </form>
+    </div>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $Nome = $_POST['Nome'];
         $Idade = $_POST['Idade'];
+        $sexo = $_POST['sexo'];
 
+        if (!empty($Nome) && !empty($Idade) && !empty($sexo)) {
 
-        if (!empty($Nome) && !empty($Idade)) {
-
-            $valid_feminino = "Sara";
-            $valid_idade = "25";
-            $valid_sexo = "feminino";// Ajustar para o sexo válido desejado.
-
-
-            if ($Nome === $valid_feminino && $Idade <= $valid_idade) {
-                echo "<script>alert('Bem-Vinda, $Nome!');</script>";
+            if ($sexo == 'feminino' && $Idade < 25) {
+                echo "Acesso permitido $Nome";
             } else {
-                echo "<script>alert('Usuário ou senha incorretos.');</script>";
+                echo "Acesso não permitido $nome";
             }
         } else {
-            echo "<script>alert('Por favor, preencha todos os campos.');</script>";
+            echo "Por favor, preencha todos os campos.";
         }
     }
     ?>
 </body>
 
 </html>
-<!-- 
-Entrar com nome, sexo e idade de uma pessoa, se a pessoa for do sexo feminino e tiver menos que 25 anos, imprimir nome e
-a mensagem: ACEITA. Caso contrário, imprimir nome e a mensagem: NÃO ACEITA. -->
